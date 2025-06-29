d2a365982e5d031e1521837d84f8e4fab15ad2f1
nika123w <nikusha.kurtiashvili@gmail.com>
2025-06-29 10:14:37 +0400

    Create V3__create_users_table.sql

---
+++ V3__create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    birthday DATE NOT NULL
+);
